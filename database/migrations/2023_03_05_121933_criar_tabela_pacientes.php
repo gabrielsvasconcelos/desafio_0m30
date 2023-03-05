b00895bf97c6723df0ecc79343edffbaaec8ef3d
Gabriel Vasconcelos <92489005+gabrielsvasconcelos@users.noreply.github.com>
2023-03-05 13:40:25 -0300

    feat: adiciona conexão com db e cria tabela pacientes
    Realiza conexão com o banco de dados mysql usando a base de dados om30
    
    Cria migration [criar_tabela_paciente] responsável por criar a tabela pacientes

---
+++ database/migrations/2023_03_05_121933_criar_tabela_pacientes.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pacientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('foto_paciente');
+            $table->string('nome');
+            $table->string('nome_mae');
+            $table->date('data_nascimento');
+            $table->integer('cpf');
+            $table->integer('cns');
+            $table->string('endereco');
+            $table->integer('cep');
+            $table->integer('numero');
+            $table->string('complemento');
+            $table->string('bairro');
+            $table->string('cidade');
+            $table->string('estado');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
